3ba4eba846917f3dfb2a896c67df5f2e3e57378c
Sina Keshvadi <webidsina@gmail.com>
2023-11-27 12:41:31 -0800

    Updates

---
+++ precache-manifest.bd2957fd5851ff97eeef963abdd23118.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f673013803f541e4d14bdb017de339cd",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "315a26b01a7815574a3b",
+    "url": "/tictactoe/static/css/main.9d171745.chunk.css"
+  },
+  {
+    "revision": "08042576b85754a9f29e",
+    "url": "/tictactoe/static/js/2.a99b76dd.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/tictactoe/static/js/2.a99b76dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a7bdc27e2ff5ea924f",
+    "url": "/tictactoe/static/js/3.e734d89f.chunk.js"
+  },
+  {
+    "revision": "315a26b01a7815574a3b",
+    "url": "/tictactoe/static/js/main.b8de1c7e.chunk.js"
+  },
+  {
+    "revision": "db1ef4d55ced39d486c3",
+    "url": "/tictactoe/static/js/runtime-main.df47c732.js"
+  }
+]);
